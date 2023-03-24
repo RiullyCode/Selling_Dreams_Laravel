c4d5f6afebc881d1737b6faeb44718aa47439cb0
Pablo <pablojimenezovd@gmail.com>
2023-03-24 17:55:56 +0100

    mapa funcionando

--- public/js/mapa.js
+++ public/js/mapa.js
@@ -1,6 +1,22 @@
+window.onload = function () {
+  initMap();
+};
+
 function initMap() {
-    // Aquí va el código para inicializar el mapa
-  }
+  var map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: 43.361915, lng: -5.849389 },
+    zoom: 14,
+    mapId: "663d3bd556eb5ea6" // Agrega esta línea para usar tu mapa personalizado
+  });
+
+  var marker = new google.maps.Marker({
+    position: { lat: -12.046373, lng: -77.042754 },
+    map: map,
+    title: "Mi máquina expendedora",
+  });
+}
+
+
 
   document.addEventListener("DOMContentLoaded", function() {
     AOS.init();
--- resources/views/mapa.blade.php
+++ resources/views/mapa.blade.php
@@ -8,14 +8,13 @@
 <link rel="stylesheet" href="{{ url('css/mapa.css') }}">
 <!-- link a JS -->
 <script src="{{ url('js/mapa.js') }}"></script>
-<!-- link al mapa googleapi -->
-<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyD0Na630R_K2PnRSlLSiNHPLelsIQQJwFM&callback=initMap" async defer></script>
 
 @endsection
 
 @section('body')
 <h1 data-aos="zoom-in">Mapa de máquinas expendedoras</h1>
-<div id="map" data-aos="fade-up" data-aos-duration="1000"></div>
+<div id="map" style="width:100%; height:500px;" data-aos="fade-up" data-aos-duration="1000"></div>
+
 <!-- Contenedor para mostrar información de la máquina al hacer clic -->
 <div id="machine-info" style="display:none;" data-aos="fade-up" data-aos-duration="1000">
   <h3>Información de la máquina</h3>
@@ -26,11 +25,9 @@
 
 @section('scripts')
   <!-- Cualquier script adicional que necesites incluir -->
+  <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBc9uaA7-k7wHPanrwYdSY7sanbhSfKibk"></script>
+  <script src="{{ url('js/mapa.js') }}"></script>
   <script>
     AOS.init();
   </script>
-@endsection
-
-@section('footer')
-  <!-- Cualquier elemento adicional que se quiera añadir al footer -->
 @endsection
